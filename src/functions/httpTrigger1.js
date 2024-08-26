e0251aad50f04d7de21810fcadd9fcdf35c0b551
miguelangelvasquez1 <vgmiguel16@gmail.com>
2024-08-26 12:00:24 -0500

    email

---
+++ src/functions/httpTrigger1.js
@@ -0,0 +1,34 @@
+const { app } = require('@azure/functions');
+const Handlebars = require('handlebars');
+const { EmailClient } = require("@azure/communication-email");
+const fs = require('fs');
+const path = require('path');
+const connectionString = "endpoint=https://emails-adso-192.unitedstates.communication.azure.com/;accesskey=GAb2fQ4gL40IxaCd71vqzemUKJ3B4qQzYcgjGeFkT5SZdwA4oSShJQQJ99AHACULyCps5mg0AAAAAZCSDFZt";
+const client = new EmailClient(connectionString);
+app.http('httpTrigger1', {
+methods: ['POST'],
+handler: async (request, context) => {
+const requestData = await request.json();
+const subject = requestData.subject;
+const templateName = requestData.templateName;
+const dataTemplate = requestData.dataTemplate;
+const to = requestData.to;
+const templatePath = path.join(__dirname, templateName);
+const source = fs.readFileSync(templatePath, 'utf-8');
+const template = Handlebars.compile(source);
+const html = template({ name: dataTemplate.name });
+const emailMessage = {
+senderAddress: "DoNotReply@20a7a68d-6cd3-4e4c-8045-b8a646fd8f7a.azurecomm.net",
+content: {
+subject: subject,
+html: html,
+},
+recipients: {
+to: [{ address: to }],
+},
+};
+const poller = await client.beginSend(emailMessage);
+const result = await poller.pollUntilDone();
+return { body: `email sent successfully` };
+}
+});
